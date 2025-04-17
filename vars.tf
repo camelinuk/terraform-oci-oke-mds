90ed2ae233512f5c9355d8c2b943b5d19e59e10e
James Chen <camelinuk@users.noreply.github.com>
2025-04-17 20:02:21 +0800

    Update vars.tf
    修改了pub key, node_shape

--- vars.tf
+++ vars.tf
@@ -5,20 +5,19 @@ variable region { }
 
 #network
 
-
 #oke
 variable kube_cluster_name { default = "oke_cluster" }
 variable kube_version { default = "v1.21.5" }
 variable node_shape { default = "VM.Standard2.1" }
 #variable node_shape_ocpus { default = 1 }
 #variable node_shape_memory { default = 8 }
-variable node_pool_ssh_public_key { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvRZ6JUfI6qGFL5Y3Ql2/9Awr3stdOTUTK2dDbvppG0f8gSACQHK6qdJkuQMESRdaTlxhBAxBxBB46C6d9v2bYSroyNhGPf3Nk2vqaV5Sg75krHqnT4UTnRaTxGj3xj6xtpBsVFABIAK7fNwrrWvza+8MLyX83rwUGlm8CPoI5a32+EUuNEiOzWnPRCtuh+rnpozyRgEQyVD/r0Y/rQIwQVrvdTn2XziIkJ8gxGn39FtswUArwuo/iJ306WXaMxYzTpVVhiw+dzKPOoCz4R0D9PsuxL3EtZeXSi8B8di2Og5caURu4E5j3lsBvUkD/mUD3JTZap9KGImVjfM7B3/zn"}
+variable node_pool_ssh_public_key { default = "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkkDoPXZg4oLfqYQ2PanS+QhaIN9dPSRskxyplK3teEjUjhriY2FnZ0NZBuuFhUm9XIKXCfZLQdWRZ4U3qAFcYQI8mvzR0xrba88e9A8gYm+j9VGkF59HOp4KBwBenKeJLDBCm0FbJ94+puVanVSKmgZa7K7boCflCSm4KshFl6/88peug9fhEpwGO6FCgvn1iH0JOasXGaC+IzJ6WCOMpjTdqWEVm0oVRPgp21odJwowRAix9V8kCOQ6Oy31wjSpXqW0nmp9Y1m3N+k6j0Yz40bVTPAL1+4s2TwuY6wh54incndNwbsppn9O7F1ZJP4moD+KO1JHCjuasgOyKYssL9e45GnPQFNIuVkCbe8LYHHK1gQe8/7Gt3GJF/i6vTUhuuRRr3gfLkyD/4WVUUSrEWjC1Dy42s91TdVlJNrROtjoNdOSVwMnQ34Uepxb4lLZYo48WlQPceoeg4pnuHUdaowBG5jly08JweUSbuJhBJo9gWo0DpMM0NAmL6/GgrrM="}
 
 #operator
-variable operator_shape { default = "VM.Standard3.Flex" }
+variable operator_shape { default = "VM.Standard.E3.Flex" }
 variable operator_shape_ocpus { default = 1 }
 variable operator_shape_memory { default = 8 }
-variable operator_ssh_public_key { default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCvRZ6JUfI6qGFL5Y3Ql2/9Awr3stdOTUTK2dDbvppG0f8gSACQHK6qdJkuQMESRdaTlxhBAxBxBB46C6d9v2bYSroyNhGPf3Nk2vqaV5Sg75krHqnT4UTnRaTxGj3xj6xtpBsVFABIAK7fNwrrWvza+8MLyX83rwUGlm8CPoI5a32+EUuNEiOzWnPRCtuh+rnpozyRgEQyVD/r0Y/rQIwQVrvdTn2XziIkJ8gxGn39FtswUArwuo/iJ306WXaMxYzTpVVhiw+dzKPOoCz4R0D9PsuxL3EtZeXSi8B8di2Og5caURu4E5j3lsBvUkD/mUD3JTZap9KGImVjfM7B3/zn"}
+variable operator_ssh_public_key { default = "sh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkkDoPXZg4oLfqYQ2PanS+QhaIN9dPSRskxyplK3teEjUjhriY2FnZ0NZBuuFhUm9XIKXCfZLQdWRZ4U3qAFcYQI8mvzR0xrba88e9A8gYm+j9VGkF59HOp4KBwBenKeJLDBCm0FbJ94+puVanVSKmgZa7K7boCflCSm4KshFl6/88peug9fhEpwGO6FCgvn1iH0JOasXGaC+IzJ6WCOMpjTdqWEVm0oVRPgp21odJwowRAix9V8kCOQ6Oy31wjSpXqW0nmp9Y1m3N+k6j0Yz40bVTPAL1+4s2TwuY6wh54incndNwbsppn9O7F1ZJP4moD+KO1JHCjuasgOyKYssL9e45GnPQFNIuVkCbe8LYHHK1gQe8/7Gt3GJF/i6vTUhuuRRr3gfLkyD/4WVUUSrEWjC1Dy42s91TdVlJNrROtjoNdOSVwMnQ34Uepxb4lLZYo48WlQPceoeg4pnuHUdaowBG5jly08JweUSbuJhBJo9gWo0DpMM0NAmL6/GgrrM="}
 variable operator_ssh_private_key_path { default = "./keys/id_rsa"}
 variable operator_os { default = "Oracle Linux" }
 variable operator_os_version { default = "7.9" }
@@ -37,12 +36,13 @@ variable "mysql_db_system_admin_username" {
 variable "mysql_db_system_admin_password" {
   description = "Password for the admin user for MySQL Database Service"
   type        = string
-  default     = "Oracle#123"
+  default     = "Jamesql##0987$$"
 }
 
 variable "mysql_shape_name" {
     #default = "MySQL.VM.Standard.E3.1.8GB"
-    default = "MySQL.HeatWave.VM.Standard.E3"
+    #default = "MySQL.HeatWave.VM.Standard.E3"
+    default = "VM.Standard.A1.Flex"
 }
 
 variable "mysql_data_storage_in_gb" {
